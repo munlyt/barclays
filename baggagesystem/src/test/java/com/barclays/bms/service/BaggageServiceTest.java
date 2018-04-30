349ee20e455de7fc04c8a4dc2526b07e047ad562
Panday, Chandra <Chandra>
2018-04-30 09:25:50 -0500

    Added compiler version to jdk 1.8 and test

--- baggagesystem/src/test/java/com/barclays/bms/service/BaggageServiceTest.java
+++ baggagesystem/src/test/java/com/barclays/bms/service/BaggageServiceTest.java
@@ -2,23 +2,35 @@ package com.barclays.bms.service;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import com.barclays.bms.engine.BaggagePathService;
-import com.barclays.bms.engine.BaggagePathServiceImpl;
+import com.barclays.bms.exception.BusinessException;
+import com.barclays.bms.model.BaggageInfo;
+import com.barclays.bms.utils.Constants;
 import com.barclays.bms.xmlparser.BaggageXMLParser;
 
 public class BaggageServiceTest {
 	private BaggageService baggageService;
 	
+	@Mock
+	BaggagePathService baggagePathService;
+	
+	@Mock
+	BaggageXMLParser xmlParser;
+	
 	@Before
 	public void setUp() {
-		BaggagePathService baggagePathService = new BaggagePathServiceImpl();
-		BaggageXMLParser xmlParser = new BaggageXMLParser();
+		MockitoAnnotations.initMocks(this);
 		baggageService = new BaggageService(baggagePathService, xmlParser);
 	}
 
 	@Test
-	public void testHandleBaggage() {
+	public void testHandleBaggage() throws BusinessException {
+		BaggageInfo baggageInfo = Mockito.mock(BaggageInfo.class);
+		Mockito.when(xmlParser.parseBaggageXML(Constants.BAGGAGE_XML)).thenReturn(baggageInfo);
 		baggageService.handleBaggage();
 	}
 
